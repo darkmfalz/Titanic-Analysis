0af33b31bce59155462d0438f746359797c85609
Adeeb Sheikh <asheikh4@u.rochester.edu>
2016-04-17 19:32:55 -0400

    Implemented the linear and quadratic classifiers.

--- classifier.R
+++ classifier.R
@@ -2,7 +2,9 @@ setwd("C:/Everything Else/Blackhat/R/Titanic")
 traindata = read.csv("train.csv")
 testdata = read.csv("test.csv")
 
-#Logistic Analysis
+#Note to self, include automatic stepwise regression ability
+
+#1. Logistic Analysis
 glm.fit = glm(Survived~Pclass+Sex+Age+Fare, family = binomial, data = traindata)
 summary(glm.fit)
 #Generate confusion matrix
@@ -10,6 +12,7 @@ glm.probs = predict(glm.fit, traindata, type="response")
 glm.pred = rep("0", NROW(traindata))
 glm.pred[glm.probs > .5] = "1"
 table(glm.pred, traindata[["Survived"]])
+
 #Removing fare, because it has such a high p-value
 glm.fit = glm(Survived~Pclass+Sex+Age, family = binomial, data = mydata)
 summary(glm.fit)
@@ -19,4 +22,11 @@ glm.pred = rep("0", NROW(traindata))
 glm.pred[glm.probs > .5] = "1"
 table(glm.pred, traindata[["Survived"]])
 
-#Linear Discriminant Analysis
+#2. Linear Discriminant Analysis
+library(MASS)
+lda.fit = lda(Survived~Pclass+Sex+Age+Fare, data = traindata)
+lda.fit
+#Generate confusion matrix
+lda.pred = predict(lda.fit, traindata)
+lda.class = lda.pred$class
+table(lda.class, traindata[["Survived"]])
