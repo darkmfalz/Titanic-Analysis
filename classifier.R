015b4a18f426019ba376f8fbd77231c7ebc5cd7d
Adeeb Sheikh <asheikh4@u.rochester.edu>
2016-04-18 00:29:44 -0400

    Restructured the R file so it's a series of functions

--- classifier.R
+++ classifier.R
@@ -3,9 +3,24 @@ rawdata = read.csv("train.csv")
 testdata = read.csv("test.csv")
 
 #Clean the gosh-diggity-darn data
-traindata = subset(rawdata, Age!="NA")
+tdata = subset(rawdata, Age!="NA")
 
-#k-Fold Cross-Validation Functions
+#Note to self, include automatic stepwise regression ability
+
+#1. Logistic Analysis
+logFit <- function(formula, traindata){
+	glm.fit = glm(formula, family = binomial, data = traindata)
+	return(glm.fit)
+}
+
+logConfusion <- function(fit){
+	#Generate confusion matrix
+	glm.probs = predict(glm.fit, traindata, type="response")
+	glm.pred = rep("0", NROW(traindata))
+	glm.pred[glm.probs > .5] = "1"
+	return(table(glm.pred, traindata[["Survived"]]))
+}
+
 logCV <- function(formula, tempdata, k){
 	#set.seed(17)
 	folds <- split(tempdata, sample(rep(1:k, NROW(tempdata)/k)))
@@ -27,12 +42,26 @@ logCV <- function(formula, tempdata, k){
 		glm.pred[glm.probs > .5] = "1"
 		currtable <- table(glm.pred, data.frame( folds[[i]] )[["Survived"]])
 		currerr <- currtable[1,2] + currtable[2,1]
-		err[i] <- currerr*NROW(tempdata)/NROW(data.frame(folds[[i]]))
+		err[i] <- currerr/NROW(data.frame(folds[[i]]))
 	}
 	summary <- vector(mode = "numeric", length = 2)
 	summary[1] <- mean(err)
 	summary[2] <- var(err)
 	return(summary)
+}
+
+#2. Linear Discriminant Analysis
+ldaFit <- function(formula, traindata){
+	library(MASS)
+	lda.fit = lda(formula, data = traindata)
+	return(lda.fit)
+}
+
+ldaConfusion <- function(fit){
+	#Generate confusion matrix
+	lda.pred = predict(lda.fit, traindata)
+	lda.class = lda.pred$class
+	return(table(lda.class, traindata[["Survived"]]))
 }
 
 ldaCV <- function(formula, tempdata, k){
@@ -55,12 +84,25 @@ ldaCV <- function(formula, tempdata, k){
 		lda.class = lda.pred$class
 		currtable <- table(lda.class, data.frame( folds[[i]] )[["Survived"]])
 		currerr <- currtable[1,2] + currtable[2,1]
-		err[i] <- currerr*NROW(tempdata)/NROW(data.frame(folds[[i]]))
+		err[i] <- currerr/NROW(data.frame(folds[[i]]))
 	}
 	summary <- vector(mode = "numeric", length = 2)
 	summary[1] <- mean(err)
 	summary[2] <- var(err)
 	return(summary)
+}
+
+#3. Quadratic Discriminant Analysis
+qdaFit <- function(formula, traindata){
+	qda.fit = qda(formula, data = traindata)
+	return(qda.fit)
+}
+
+qdaConfusion <- function(fit){
+	#Generate confusion matrix
+	qda.pred = predict(qda.fit, traindata)
+	qda.class = qda.pred$class
+	return(table(qda.class, traindata[["Survived"]]))	
 }
 
 qdaCV <- function(formula, tempdata, k){
@@ -83,55 +125,10 @@ qdaCV <- function(formula, tempdata, k){
 		qda.class = qda.pred$class
 		currtable <- table(qda.class, data.frame( folds[[i]] )[["Survived"]])
 		currerr <- currtable[1,2] + currtable[2,1]
-		err[i] <- currerr*NROW(tempdata)/NROW(data.frame(folds[[i]]))
+		err[i] <- currerr/NROW(data.frame(folds[[i]]))
 	}
 	summary <- vector(mode = "numeric", length = 2)
 	summary[1] <- mean(err)
 	summary[2] <- var(err)
 	return(summary)
 }
-
-#Note to self, include automatic stepwise regression ability
-
-#1. Logistic Analysis
-glm.fit = glm(Survived~Pclass+Sex+Age+Fare, family = binomial, data = traindata)
-summary(glm.fit)
-#Generate confusion matrix
-glm.probs = predict(glm.fit, traindata, type="response")
-glm.pred = rep("0", NROW(traindata))
-glm.pred[glm.probs > .5] = "1"
-table(glm.pred, traindata[["Survived"]])
-#Some k-fold cross-validation stuff
-logCV(Survived~Pclass+Sex+Age+Fare, traindata, 10)
-
-#Removing fare, because it has such a high p-value
-glm.fit = glm(Survived~Pclass+Sex+Age, family = binomial, data = mydata)
-summary(glm.fit)
-#Generate confusion matrix
-glm.probs = predict(glm.fit, traindata, type="response")
-glm.pred = rep("0", NROW(traindata))
-glm.pred[glm.probs > .5] = "1"
-table(glm.pred, traindata[["Survived"]])
-#Some k-fold cross-validation stuff
-logCV(Survived~Pclass+Sex+Age, traindata, 10)
-
-#2. Linear Discriminant Analysis
-library(MASS)
-lda.fit = lda(Survived~Pclass+Sex+Age, data = traindata)
-lda.fit
-#Generate confusion matrix
-lda.pred = predict(lda.fit, traindata)
-lda.class = lda.pred$class
-table(lda.class, traindata[["Survived"]])
-#Some k-fold cross-validation stuff
-ldaCV(Survived~Pclass+Sex+Age, traindata, 10)
-
-#3. Quadratic Discriminant Analysis
-qda.fit = qda(Survived~Pclass+Sex+Age, data = traindata)
-qda.fit
-#Generate confusion matrix
-qda.pred = predict(lda.fit, traindata)
-qda.class = qda.pred$class
-table(qda.class, traindata[["Survived"]])
-#Some k-fold cross-validation stuff
-qdaCV(Survived~Pclass+Sex+Age, traindata, 10)
